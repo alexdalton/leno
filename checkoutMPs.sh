9fb22e05d9ec4833527017691cf687e14c790ed2
alexdalton <alexmdalton@gmail.com>
2014-09-09 17:26:33 -0500

    Create checkoutMPs.sh

---
+++ checkoutMPs.sh
@@ -0,0 +1,47 @@
+#!bin/bash
+
+#This program checks out the correct MP from the students directory
+#in subversion
+
+$roster
+$date
+$baseDir
+$mp
+
+while getopts ":b:d:m:r:" opts
+do
+  case $opts in
+    b)
+      baseDir=$OPTARG
+      ;;
+    d)
+       date=$OPTARG
+       ;;
+    m)
+      mp=$OPTARG
+      ;;
+    r)
+      roster=$OPTARG
+      ;;
+    \?)
+      echo "Invalid Argument"
+      exit 1
+      ;;
+    :)
+      echo "Option $OPTARG missing an argment"
+      exit 1
+      ;;
+  esac
+done
+
+mkdir $baseDir/$mp
+cd $baseDir/$mp
+
+while read line
+do
+  mkdir $line
+  cd $line
+  echo "Checking out $line"
+  svn co -r {"$date"} https://subversion.ews.illinois.edu/svn/sp14-ece198kl/$line/$mp .
+  cd ..
+done < $roster
