37bf7b4d3fdf77f270e2f9335378cefbffc66af5
alexdalton <alexmdalton@gmail.com>
2015-12-02 19:46:52 -0600

    finished Hw3

--- speech/HW3/run.m
+++ speech/HW3/run.m
@@ -1,11 +1,20 @@
-load sentence1.mat
+load sentence3.mat
 
 [yEncoded, delta, error] = encodeLDM(y, .9, 2);
 
 x_hat = decodeLDM(yEncoded, delta, .9);
 
 figure;
-subplot(2, 1, 1);
+subplot(3, 1, 1);
 plot(y);
-subplot(2, 1, 2);
+title('Original input signal');
+
+subplot(3, 1, 2);
+plot(error);
+title('Error signal e(n)');
+
+subplot(3, 1, 3);
 plot(x_hat);
+title('Decoded signal after LDM encoding');
+
+10 * log10(sum(y.^2) / sum(error.^2))
